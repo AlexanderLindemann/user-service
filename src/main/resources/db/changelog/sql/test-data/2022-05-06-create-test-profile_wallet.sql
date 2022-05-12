3adfc36f6a600105c6cb5b47e318e5dbb17fe359
Vadim Margiev <vmargiev@okta-solutions.tech>
2022-05-12 17:42:58 +0300

    user-service: remove test-data

--- src/main/resources/db/changelog/sql/test-data/2022-05-06-create-test-profile_wallet.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO public.profile_wallet (id, experience_count, coin_balance, user_profile_id, celebrity_id, created_at,
-                                   updated_at, created_by, updated_by, vote_balance, period_id, subscriber,
-                                   wheel_balance, nft_votes_balance)
-VALUES (DEFAULT, DEFAULT, 100, '7038e63a-0da6-4cf8-b7b5-f0e5a5de55f9',
-        '786fca71-0646-4984-9420-cb6c3941d967', '2022-05-06 16:08:16.000000',
-        '2022-05-06 16:08:17.000000', '2022-05-06 16:08:16.000000', null,
-        100, null, DEFAULT, 100, 100)
