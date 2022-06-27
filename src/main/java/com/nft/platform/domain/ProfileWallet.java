90639abd92c5d9b792bef91e0afdd4c9cc16ffee
alexander_l <idsmokedried@gmail.com>
2022-06-27 18:03:59 +0300

    bug/fix-try4

--- src/main/java/com/nft/platform/domain/ProfileWallet.java
+++ src/main/java/com/nft/platform/domain/ProfileWallet.java
@@ -2,11 +2,9 @@ package com.nft.platform.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -15,15 +13,12 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.Table;
 import java.util.UUID;
 
 @AllArgsConstructor
 @Builder
-@Getter
-@Setter
+@Data
 @Entity
-@Table(name = "profile_wallet")
 @NamedQueries({
         @NamedQuery(name = "ProfileWallet.findAllCelebrityFans", query = "SELECT up " +
                 "            FROM ProfileWallet up " +
@@ -42,28 +37,15 @@ public class ProfileWallet extends BaseEntity {
 
     @Id
     @GeneratedValue
-    @Column(name = "id", nullable = false)
     private UUID id;
-
-    @Column(name = "experience_count", nullable = false)
     private int experienceCount;
-
-    @Column(name = "vote_balance", nullable = false)
     private int voteBalance;
-
-    @Column(name = "wheel_balance", nullable = false)
     private int wheelBalance;
-
-    @Column(name = "coin_balance", nullable = false)
     private long coinBalance;
-
-    @Column(name = "nft_votes_balance", nullable = false)
     private int nftVotesBalance;
-
-    @Column(name = "subscriber", nullable = false)
     private boolean subscriber;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "celebrity_id", nullable = false)
     private Celebrity celebrity;
 
