2a4ffe677ae6300f7d5f8cfb56b45c15d4de096c
Vladimir Andreev <v.andreev@platform.live>
2022-07-19 18:54:29 +0300

    NFD-444. Added wheel balance update.

--- src/main/java/com/nft/platform/service/ChallengeRewardService.java
+++ src/main/java/com/nft/platform/service/ChallengeRewardService.java
@@ -1,16 +1,17 @@
 package com.nft.platform.service;
 
-import com.nft.platform.challengeservice.api.dto.enums.AwardType;
 import com.nft.platform.challengeservice.api.dto.response.AdditionalRewardsResponseDto;
 import com.nft.platform.challengeservice.api.event.KafkaChallengeCompletedEvent;
 import com.nft.platform.repository.ProfileWalletRepository;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
 @Slf4j
+@Service
 @RequiredArgsConstructor
 public class ChallengeRewardService {
 
@@ -20,19 +21,32 @@ public class ChallengeRewardService {
     public void handleRewards(KafkaChallengeCompletedEvent event) {
         log.info("Will handle challenge rewards");
         for (AdditionalRewardsResponseDto rewardResponseDto : event.getRewards()) {
-            if (rewardResponseDto.getAwardType() == AwardType.COINS) {
-                profileWalletRepository.updateProfileWalletCoinBalance(
+            switch (rewardResponseDto.getAwardType()) {
+
+                case COINS:
+                    profileWalletRepository.updateProfileWalletCoinBalance(
                         event.getUserId(),
                         event.getCelebrityId(),
                         rewardResponseDto.getQuantity()
-                );
-            }
-            if (rewardResponseDto.getAwardType() == AwardType.SUBSCRIBE) {
-                profileWalletRepository.updateProfileWalletSubscription(
+                    );
+                break;
+
+                case SUBSCRIBE:
+                    profileWalletRepository.updateProfileWalletSubscription(
                         event.getUserId(),
                         event.getCelebrityId(),
                         true
-                );
+                    );
+                break;
+
+                case FORTUNE_WHEEL:
+                    profileWalletRepository.updateProfileWalletWheelBalance(
+                        event.getUserId(),
+                        event.getCelebrityId(),
+                        rewardResponseDto.getQuantity()
+                    );
+                break;
+
             }
         }
     }
