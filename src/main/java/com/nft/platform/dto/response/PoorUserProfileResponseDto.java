2602a42bbd1f4b398d2733d13089294595cadecd
Sergey Tyulnikov <sergey.tulnikhov@itpo.group>
2022-08-02 13:35:16 +0300

    NFD-1626 Corrections in poor dto

--- src/main/java/com/nft/platform/dto/response/PoorUserProfileResponseDto.java
+++ src/main/java/com/nft/platform/dto/response/PoorUserProfileResponseDto.java
@@ -7,21 +7,33 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Data
 @SuperBuilder
-@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
-public class PoorUserProfileResponseDto extends MinimizedAbstractUserProfileDto {
+public class PoorUserProfileResponseDto {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String nickname;
+
+    private Boolean invisibleName;
+
+    private String description;
+
+    private String site;
+
+    private String facebookId;
+
+    private String twitterId;
 
     private String googleId;
 
     private String imageUrl;
 
     private String imagePromoBannerUrl;
-
-    private UUID keycloakUserId;
 
     private LocalDateTime createdAt;
 
