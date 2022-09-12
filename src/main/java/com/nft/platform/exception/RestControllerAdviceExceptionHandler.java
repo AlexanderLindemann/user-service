2c5d0e3fa600ed25d5f8a6b4716ab18e5aa32ac9
Mikhail Cherevko <cherevko.mikhail@itpo.group>
2022-09-12 14:21:42 +0000

    No key: Fix ordering for Controller Advices

--- src/main/java/com/nft/platform/exception/RestControllerAdviceExceptionHandler.java
+++ src/main/java/com/nft/platform/exception/RestControllerAdviceExceptionHandler.java
@@ -2,7 +2,7 @@ package com.nft.platform.exception;
 
 import feign.FeignException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.NestedExceptionUtils;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
+@Order(1)
 public class RestControllerAdviceExceptionHandler {
 
     @ExceptionHandler(RestException.class)
@@ -19,8 +20,7 @@ public class RestControllerAdviceExceptionHandler {
         var exceptionDto = ExceptionDto.builder()
                 .message(ex.getMsg())
                 .httpStatus(ex.getHttpStatus())
-                .build()
-                ;
+                .build();
 
         return ResponseEntity
                 .status(ex.getHttpStatus())
@@ -36,26 +36,11 @@ public class RestControllerAdviceExceptionHandler {
         var exceptionDto = ExceptionDto.builder()
                 .message("Error occurred while " + ex.request().httpMethod() + " to '" + ex.request().url() + ex.getMessage() + "': " + ex.contentUTF8())
                 .httpStatus(status)
-                .build()
-                ;
+                .build();
 
         return ResponseEntity
                 .status(status)
                 .body(exceptionDto)
                 ;
-    }
-
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ExceptionDto> handleRuntimeException(RuntimeException ex) {
-        log.error("Caught runtime exception {}", ex.toString());
-        String message = NestedExceptionUtils.getMostSpecificCause(ex).getMessage();
-        ExceptionDto exceptionDto = ExceptionDto.builder()
-                .message(message)
-                .httpStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-                .build();
-
-        return ResponseEntity
-                .status(500)
-                .body(exceptionDto);
     }
 }
